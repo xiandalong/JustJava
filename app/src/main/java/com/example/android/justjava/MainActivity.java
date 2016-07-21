4f9c18c22b165c2072608c0c5e0fc82dfb4f9218
Xianda Long <kelvinlong8897@gmail.com>
2016-07-21 16:43:18 -0400

    Change the output to be more formal

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -20,7 +20,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Free";
+        String priceMessage = "Total: $" + quantity * 5;
+        priceMessage += "\nThank you!";
         displayMessage(priceMessage);
     }
 
