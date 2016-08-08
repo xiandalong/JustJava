46e8849fd103409e6f792fad2a9bdfa22cd036b0
Xianda Long <kelvinlong8897@gmail.com>
2016-08-08 10:41:32 -0400

    Add calculatePrice() method

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
-import java.text.NumberFormat;
-
 
 public class MainActivity extends AppCompatActivity {
     int quantity = 2;
@@ -20,7 +18,8 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage = "Total: $" + quantity * 5;
+        int price = calculatePrice();
+        String priceMessage = "Total: $" + price;
         priceMessage += "\nThank you!";
         displayMessage(priceMessage);
     }
@@ -34,13 +33,7 @@ public class MainActivity extends AppCompatActivity {
         quantityTextView.setText("" + number);
     }
 
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+
 
     public void increment(View view) {
         quantity += 1;
@@ -58,5 +51,17 @@ public class MainActivity extends AppCompatActivity {
     private void displayMessage(String message) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(message);
+    }
+
+    private int calculatePrice(int number) {
+        return number * 5;
+    }
+
+    private int calculatePrice(int number, int unitPrice) {
+        return number * unitPrice;
+    }
+
+    private int calculatePrice() {
+        return quantity * 5;
     }
 }
